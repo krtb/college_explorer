a4741b9a5a8abc3e2f257d7bd26bb788f014ae8f
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-03 15:56:03 -0500

    add route /hello, res.send msg Hello World

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,14 @@ app.use(bodyParser.json())
 app.use(cors())
 
 // log message to console on Express.js start
-console.log("Hello, world!");
+app.get('/hello', (req, res) => {
+    res.send(
+        [{
+            title: "Hello World",
+            description: "Hi there! This is the college_explorer app!"
+        }]
+    )
+})
 
 // Server listening on PORT 8081
 app.listen(process.env.PORT || 8081)
