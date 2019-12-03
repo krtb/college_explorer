110a17a896e8bb2cd4ee5641212da1efbd2075eb
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-03 15:46:49 -0500

    add inline comments describing code purpose

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,13 @@
-
+// import node modules
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+// instantiate express
 const app = express()
 
-
+// set express to use HTTP request helpers
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
