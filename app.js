b13919390db3c2eb8d52e7210d6acf2ec82f5919
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-02 22:29:14 -0500

    use listen method to add server, port 3000

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+
+app.listen(3000, function () {
+    //  responds with'404 Not Found' for every path that is not root, '/'
+    console.log('college_explorer app ==> listening on port, 3000!');
+});
