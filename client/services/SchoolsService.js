a9c2abfb6c04a9e22d086426227cb12c76ebba1d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-04 14:30:48 -0500

    will hit schools api endpoint in server folder

---
+++ client/services/SchoolsService.js
@@ -0,0 +1,9 @@
+// @ = current src folder
+// uses resolve.alias, from Webpack: client/build/webpack.base.conf.js
+import API from '@/services/Api'
+
+export default {
+    fetchSchools() {
+        return Api().get('schools')
+    }
+}
