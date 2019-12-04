6e051be62ec534ae5d89748aca5980482e75f7ee
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-04 14:39:38 -0500

    add new page/route for displaying Schools

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/schools',
+      name: 'Schools',
+      component: Schools
     }
   ]
 })
