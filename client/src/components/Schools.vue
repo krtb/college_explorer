8fc74607208cb1c10ffcddf2c7637d14fbf32e5e
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-05 12:19:09 -0500

    render colleges from express/node back-end server

---
+++ client/src/components/Schools.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="schools">
+    <h1>Schools</h1>
+    This file will list all the schools.
+    {{ schools }}
+  </div>
+</template>
+
+<script>
+import SchoolsService from '../../services/SchoolsService'
+export default {
+  name: 'schools',
+  data () {
+    return {
+      schools: []
+    }
+  },
+  mounted () {
+    this.getSchools()
+  },
+  methods: {
+    async getSchools () {
+      try {
+          const response = await SchoolsService.fetchSchools()
+          this.schools = response.data
+      } catch (e) {
+        console.log(e, "THERE WERE ERRORS")
+      }
+    }
+  }
+}
+</script>
