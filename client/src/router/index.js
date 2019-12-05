54de0268dac0009cb1db4d34809296aa3b76d218
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-05 12:19:53 -0500

    alter import paths, remove @ symbol

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
+import Schools from '../components/Schools'
 
 Vue.use(Router)
 
