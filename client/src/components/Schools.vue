69ac975beb439e2e1f91f2d9a007fef7c2a0a28b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-05 19:45:12 -0500

    add sortedArray function, sort names alphabetic.

--- client/src/components/Schools.vue
+++ client/src/components/Schools.vue
@@ -5,7 +5,7 @@
 
     This file will list all the schools.
      
-    <div v-for="school in schools" v-bind:key="school.id">
+    <div v-for="school in sortedArray" v-bind:key="school.id">
       <p>
         <span><b>School: {{ school["school.name"] }}</b></span><br />
         <span>Admission Rate: {{ school["latest.admissions.admission_rate.overall"] }}</span>
@@ -30,11 +30,29 @@ export default {
   methods: {
     async getSchools () {
       try {
+        // try to fetch from Node + Express backe-nd
           const response = await SchoolsService.fetchSchools()
+          //sets fetched data to schools array, defined in data above
           this.schools = response.data
       } catch (e) {
         console.log(e, "THERE WERE ERRORS")
       }
+    }
+  },
+   computed: {
+    sortedArray: function() {
+      function compare(a, b) {
+        // compares the school name value of first and following item in array
+        if (a["school.name"] < b["school.name"])
+          return -1;
+        if (a["school.name"] > b["school.name"])
+          return 1;
+        // return sorted name
+        return 0;
+      }
+
+      // returns an alphabetically sorted array
+      return this.schools.sort(compare);
     }
   }
 }
