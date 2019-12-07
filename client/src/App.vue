1a39c869ac92b5dfcb9e95642827a4f5d445d421
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-07 09:59:10 -0500

    add working slide from bootstrap-vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -24,7 +24,68 @@
     </b-collapse>
   </b-navbar>
 </div>
-    <img src="./assets/logo.png">
+
+
+    <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template v-slot:img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="https://picsum.photos/1024/480/?image=55"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+    </b-carousel>
+
+    <p class="mt-4">
+      Slide #: {{ slide }}<br>
+      Sliding: {{ sliding }}
+    </p>
+  </div>
+
+
+
     <router-view/>
   </div>
 </template>
@@ -39,9 +100,17 @@ import { BNavbar,
           BNavItem,
           BNavbarToggle,
           BNavItemDropdown,
-          BFormInput
+          BFormInput,
+          BCarousel,
+          BCarouselSlide
         } from 'bootstrap-vue'
 export default {
+  data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
   name: 'App',
   components: {
     'b-navbar': BNavbar,
@@ -55,8 +124,18 @@ export default {
     'b-nav-item-dropdown': BNavItemDropdown,
     'b-dropdown-item': BNavItemDropdown,
     'b-form-input': BFormInput,
-    'b-nav-item': BNavItem
+    'b-nav-item': BNavItem,
+    'b-carousel': BCarousel,
+    'b-carousel-slide': BCarouselSlide
   },
+   methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
+    }
 }
 </script>
 
